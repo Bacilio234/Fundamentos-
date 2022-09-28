ac898778a674702a3f645d66b4a3fc3b2b05b882
Bacilio234 <pedro.bacilio.valencia@utelvt.edu.ec>
2022-09-28 17:07:52 -0400

    programa de velocímetro.cpp modificado

---
+++ velocímetro.cpp
@@ -0,0 +1,17 @@
+//pedrobacilio 
+//modificado 28-09-2022 
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	float p,b,v;
+	cout<<"Ingrese la distancia(en km) p= "; cin>>p;
+	cout<<"Ingrese el tiempo(en horas)  b= "; cin>>b;
+	v=p/b;
+	cout<<"La velocidad calculada es de v= "<<v<<endl<<endl;
+	return 0;
+
+
+
+}
