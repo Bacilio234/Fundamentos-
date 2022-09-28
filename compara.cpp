280e8885540010781de58d09606e21bba771859b
Bacilio234 <pedro.bacilio.valencia@utelvt.edu.ec>
2022-09-28 19:46:53 -0400

    programa de compara cpp modificado

---
+++ compara.cpp
@@ -0,0 +1,28 @@
+//pedrobacilio 
+//modificado 28-09-2022 
+#include<iostream>
+using namespace std;
+int main()
+{
+	float p,b;
+	cout<<"ingrese p b:"; cin>>p>>b;
+	if(p>b)
+	{
+	  cout<<p<<" es mayor p "<<b<<endl;
+
+
+	}else{
+
+	  cout<<b<<" es mayor p "<<p<<endl;
+
+
+	}
+
+	return 0;
+
+
+
+
+
+
+}
