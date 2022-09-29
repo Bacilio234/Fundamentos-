eab72fcee27cb38ceb044f01bda1d405694763a1
Bacilio234 <pedro.bacilio.valencia@utelvt.edu.ec>
2022-09-28 20:39:36 -0400

    programa de suma.cpp

---
+++ suma.cpp
@@ -0,0 +1,18 @@
+//pedrobacilio 
+//modificado 28-09-2022 
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+ float p,b,v;
+ cout<<"Ingrese 2 numero p b :";
+ cin>>p>>b;
+ v=p+b;
+ cout<<"El resutado es : ";
+ cout<<v<<endl;
+
+ return 0;
+
+}
